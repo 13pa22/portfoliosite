cad83a0a81d05d3ee5ba10203fc6e30633c393b8
13pa22 <32932581+13pa22@users.noreply.github.com>
2019-08-21 16:25:26 -0400

    Updates

---
+++ precache-manifest.ea172e179b16715ee92f61e6efce793a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67ae2aef62f86a5a556027c0f9ab762e",
+    "url": "/portfoliosite/index.html"
+  },
+  {
+    "revision": "a75281ec8d6ade57358b",
+    "url": "/portfoliosite/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "89b99c4d928c535ba0a9",
+    "url": "/portfoliosite/static/css/main.9836c152.chunk.css"
+  },
+  {
+    "revision": "a75281ec8d6ade57358b",
+    "url": "/portfoliosite/static/js/2.c1664eb2.chunk.js"
+  },
+  {
+    "revision": "89b99c4d928c535ba0a9",
+    "url": "/portfoliosite/static/js/main.b828ad7a.chunk.js"
+  },
+  {
+    "revision": "be7e87888dbe15e880ed",
+    "url": "/portfoliosite/static/js/runtime~main.6dde9229.js"
+  }
+]);
