b5ca55d89a4e484c3f41c3788f62d28855e7aca6
13pa22 <32932581+13pa22@users.noreply.github.com>
2019-08-21 21:33:51 -0400

    fixing landing page error

--- src/components/main.js
+++ src/components/main.js
@@ -7,7 +7,7 @@ import Projects from './projects';
 import Resume from './resume';
  const Main = () => (
  	<Switch>
- 		<Route exact path= "/"  component = {Landing} />
+ 		<Route exact path= "/portfoliosite"  component = {Landing} />
  		 <Route path= "/contact" component = {Contact} />
 		<Route  path= "/projects" component = {Projects} />
  		<Route  path= "/resume" component = {Resume} />
